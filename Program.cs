f2b6b39e1caad36d421ea61b657dd2f90175bd2f
Luca Raymaekers <luca@spacehb.net>
2025-11-21 14:52:16 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Raylib_cs;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 600, "TEST");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.RayWhite);
+
+                Raylib.DrawText("Hello C# Window", 10, 10, 20, Color.Red);
+
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
